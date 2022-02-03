6951542959cd2e45bae8790f6e3f666c745fefbd
malcolm <mlarisdjokovic@fwparker.org>
2022-02-03 14:40:54 -0600

    Worked on fucntions activity, took 45 mins I am confused on what to do

---
+++ Notes/functions.pi.py
@@ -0,0 +1,27 @@
+"""
+Functions Notes
+"""
+"""
+def max(x,y):
+    if x > y:
+        return x
+    elif x == y:
+        return x
+    else: # Only possible thing
+        return y
+biggest = max(6,2)
+print(max(6,6) + max(6,2))
+"""
+"""
+
+def sum(x,y):
+    return(x + y)
+answer = sum(4,5)
+print(sum(4,5))
+
+"""
+
+def avg(x,y):
+    addition = (x + y)
+    return addition/2
+print(avg(2,3))
