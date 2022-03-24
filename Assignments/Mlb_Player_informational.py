f4650474f702d68ef537a6cbb188500a488f45ef
malcolm <mlarisdjokovic@fwparker.org>
2022-03-24 15:03:00 -0500

    made into function

---
+++ Assignments/Mlb_Player_informational.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from pprint import pprint
+def find_in_dict(dict, stat):
+    return dict.get("sport_career_hitting").get("queryResults").get("row").get(stat)
+
+
+#if statements prints below
+def find_out():
+    if "batting".lower() in response_list:
+        print(player_name + "'s career batting average is: " + find_in_dict(stats,"avg") + "\n")
+    if "gidp".lower() in response_list:
+        print(player_name + ("'s career ground-outs into double plays are: ") + find_in_dict(stats,"gidp") + "\n")
+    if "bat".lower() in response_list:
+        print(player_name + "'s career at bat is: " + find_in_dict(stats,"ab") + "\n")
+    if "runs".lower() in response_list:
+        print(player_name + "'s career home runs are: " + find_in_dict(stats, "hr") + "\n")
+    if "left".lower() in response_list:
+        print(player_name + "'s career left-on-base are: " + find_in_dict(stats, "lob") + "\n")
+    if "babip".lower() in response_list:
+         print(player_name + "'s career batting average with a ball in play is: " + find_in_dict(stats, "babip") + "\n")
+    if "rbi".lower() in response_list:
+        print(player_name + "'s career rbi is: " + find_in_dict(stats, "rbi") + "\n")
+    if "slugging".lower() in response_list:
+        print(player_name + "'s career slugging average is: " + find_in_dict(stats, "slg") + "\n")
+
+print("Welcome to the MLB Chatbot, you can ask me a variety of things about specifc players")
+print("You can ask about batting and baserunning stats or profile, chances are, I will know. (leave out pitching stats)")
+print("Here is a list of everything you can ask me:\n "
+      "career at bats \n career gidp \n career home runs \n career left on bases \n career babip \n career rbi \n career slugging")
+print("\n")
+
+while True:
+    while True:
+        active_or_history = input("Is the player you're asking about active or retired?: ")
+        if active_or_history.lower() in ["current", "active"]:
+            y_or_n =  "Y"
+            break
+        elif active_or_history.lower() in ["historical", "retired", "old",]:
+            y_or_n = "N"
+            break
+        else:
+            print("That's not a valid response")
+
+    player_name = input("Who would you like to know about?: ")
+    user_response = input("What would you like to know?: ")
+
+
+    response_list = user_response.split()
+    player_api = requests.get( "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='"+ y_or_n + "'&name_part='" + player_name +"%25'")
+    data_dict = json.loads(player_api.text)
+    player_id = data_dict.get("search_player_all").get("queryResults").get("row").get("player_id")
+
+
+    hitting_statsapi = requests.get("http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='" + player_id + "'")
+    #pprint(json.loads(hitting_statsapi.text))
+    stats = json.loads(hitting_statsapi.text)
+    find_out()
+    yes_or_no = input("Would you like to know about another player?: ")
+    if yes_or_no.lower() == "no":
+        break
+    else:
+        print('\n')
--- Notes/netflix.reading.py
+++ Notes/netflix.reading.py
@@ -5,12 +5,10 @@ netflix_data = open('netflix.csv')
 dict_reader = csv.DictReader(netflix_data)
 #pprint(list(dict_reader))
 
-show_title = input("What show do you want to know about?: ")
-show_info = input("What would you like to know about it?: ")
+show_title = input("What show do you want to know about: ")
+show_info = input("What would you like to know about it: ")
 
 for i in (list(dict_reader)):
     if i.get('Title') == show_title:
         print(i[show_info])
         break
-    else:
-        print("Not in list")
